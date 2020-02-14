d2b04dce83fb8c62a7d2d83da3632ec1d43fc08a
Zach Mommaerts <zmommaerts@gmail.com>
2020-02-14 14:35:30 -0600

    added all four views and the controller

---
+++ SuperHeroCreator/SuperHeroCreator/Data/Migrations/20200214200738_FixedSpellingErrors.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeroCreator.Data.Migrations
+{
+    public partial class FixedSpellingErrors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
